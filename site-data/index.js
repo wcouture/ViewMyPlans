105920fd51963dbde9f13223a2ce6a2a910f46c5
William Couture <wcouture17@gmail.com>
2024-06-10 11:19:52 -0400

    Add order form submit

--- site-data/index.js
+++ site-data/index.js
@@ -77,6 +77,11 @@ app.get("/order", (req, res) => {
     res.send(full_page);
 });
 
+app.post("order-request", (req, res) => {
+    console.log(req.body);
+    res.send(JSON.stringify({ "status": "success" }));
+})
+
 app.get("/plans", (req, res) => {
     res.sendFile("/pages/plans.html", { root: __dirname });
 });
